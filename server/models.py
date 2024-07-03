60aebce42d407eb49985ecc94a56d19e1d49480c
olabayoji <bayoji7@gmail.com>
2024-07-03 21:24:46 +0100

    comment and post model

--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from database import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from routers.types.custom_types import GenderEnum, str_20
+from routers.types.custom_types import str_20
 
 
 class Base(DeclarativeBase):
@@ -18,8 +18,37 @@ class User(Base):
     date_created: Mapped[datetime] = mapped_column(default=datetime.now)
     gender: Mapped[str_20] = mapped_column()
 
-    def __init__(self, username: str, email: str, password: str, gender: GenderEnum):
-        self.username = username
-        self.email = email
-        self.password = password
-        self.gender = gender
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="author")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="author")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    content: Mapped[str] = mapped_column()
+    date_created: Mapped[datetime] = mapped_column(default=datetime.now)
+    date_updated: Mapped[datetime] = mapped_column(
+        default=datetime.now, onupdate=datetime.now
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column()
+    date_created: Mapped[datetime] = mapped_column(default=datetime.now)
+    date_updated: Mapped[datetime] = mapped_column(
+        default=datetime.now, onupdate=datetime.now
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
+
+    author: Mapped["User"] = relationship("User", back_populates="comments")
+    post: Mapped["Post"] = relationship("Post", back_populates="comments")
